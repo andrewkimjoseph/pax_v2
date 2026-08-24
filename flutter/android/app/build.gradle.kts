c754035eb4aba02c173edc1ce6081f7f04ac8a12
Andrew Kim Joseph <andrewkimjoseph@gmail.com>
2026-08-24 14:17:37 +0300

    chore: set ndkVersion to  28.2.13676358

--- flutter/android/app/build.gradle.kts
+++ flutter/android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
         applicationId = "app.thepax.android"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        ndkVersion = "27.0.12077973"
+        ndkVersion = "28.2.13676358"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
